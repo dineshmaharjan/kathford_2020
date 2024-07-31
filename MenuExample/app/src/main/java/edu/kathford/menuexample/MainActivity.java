e741f293eb3d13c21330901b597a9ff7c4dd6fd0
Dinesh Maharjan <www.maharjandinesh@gmail.com>
2024-07-31 07:51:06 +0545

    menu example completed

--- MenuExample/app/src/main/java/edu/kathford/menuexample/MainActivity.java
+++ MenuExample/app/src/main/java/edu/kathford/menuexample/MainActivity.java
@@ -1,9 +1,14 @@
 package edu.kathford.menuexample;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -12,10 +17,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView contentTextView;
+    private Button actionButton;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        contentTextView = findViewById(R.id.dummyTextView);
+        actionButton = findViewById(R.id.actionButton);
+
+        registerForContextMenu(contentTextView);
+
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this,
+                        actionButton);
+                MenuInflater inflater =popupMenu.getMenuInflater();
+                inflater.inflate(R.menu.popup_menu, popupMenu.getMenu());
+                popupMenu.show();
+
+                //handle popup menu item click listener
+                //popupMenu.setOnMenuItemClickListener();
+            }
+        });
     }
 
 
@@ -37,5 +64,26 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.select){
+            Toast.makeText(this, "Select item clicked", Toast.LENGTH_SHORT).show();
+        }else if(item.getItemId() == R.id.copy){
+            Toast.makeText(this, "Copy item clicked", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Cut item clicked", Toast.LENGTH_SHORT).show();
+        }
+        return super.onContextItemSelected(item);
     }
 }
